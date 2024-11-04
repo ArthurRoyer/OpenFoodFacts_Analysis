782df1d0465874626f51dc233ab71cdf16ba6fe4
AxelMunerol <axel.munerol@gmail.com>
2024-11-04 11:22:34 +0100

    Update routes.py
    Import pickle

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, jsonify
 from app.graphs import create_graph
 import numpy as np
 import pandas as pd
+import pickle
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
